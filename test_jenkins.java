93b6c34315230a1c7a27376989c4df0fe3f03bd0
arunbalajii <arun.balaji5005@gmail.com>
2018-03-22 12:26:32 +0530

    Add files via upload

---
+++ test_jenkins.java
@@ -0,0 +1,30 @@
+class Student
+{
+	public String env;
+	public String name;
+	Student(String env, String name)
+	{
+		this.env = env;
+		this.name = name;
+	}
+}
+
+public class GFG
+{
+	public static void main (String[] args)
+	{
+		Student[] arr = new Student[9];
+		arr[0] = new Student("sita","aman");
+		arr[1] = new Student("sita","vaibhav");
+		arr[2] = new Student("sitb","shikar");
+		arr[3] = new Student("sitci","dharmesh");
+		arr[4] = new Student("sitd","mohit");
+arr[5] = new Student("sita","vaibhav");
+		arr[6] = new Student("sitb","shikar");
+		arr[7] = new Student("sitci","dharmesh");
+		arr[8] = new Student("sitd","mohit");
+		for (int i = 0; i < arr.length; i++)
+			System.out.println( i + " : " +
+						arr[i].env +" "+ arr[i].name);
+	}
+}
